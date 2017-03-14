7afcfe89595288121e5ed38ccf32e2a0275778bb
vincent50117 <vincent50117@gmail.com>
2017-03-14 17:52:59 +0800

    debug Main & Network

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void start() {
 
-		Grahpics.start();
+		Graphics.start();
 		Network.start();
 		control.start();
 		Logic.start();
--- Network.java
+++ Network.java
@@ -1,5 +1,5 @@
 public class Network {
     public static void start() {
-        system.out.println("Network");
+        System.out.println("Network");
     }
 }
